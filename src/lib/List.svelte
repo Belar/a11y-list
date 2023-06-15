a35852ad8da3ceb80d28c783a6401b2d1bafbcad
Belar <belar@belardesign.com>
2023-06-15 12:03:02 +0000

    feat: add list of deletable entries

---
+++ src/lib/List.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import Button from './Button.svelte';
+
+	const dispatch = createEventDispatcher();
+
+	type Entry = {
+		name: string;
+	};
+
+	export let list: Entry[] = [];
+
+	function dispatchDelete(entry: Entry) {
+		dispatch('delete', entry);
+	}
+</script>
+
+<ul>
+	{#each list as entry}
+		{@const { name } = entry}
+		<li>{name} <Button on:click={() => dispatchDelete(entry)}>Delete</Button></li>
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import List from '$lib/List.svelte';
+
+	let list = [
+		{
+			name: 'Keys'
+		},
+		{
+			name: 'Bag'
+		},
+		{
+			name: 'Phone'
+		}
+	];
+
+	function deleteEntry(entry: (typeof list)[number]) {
+		const listUpdated = list.filter((existingEntry) => existingEntry !== entry);
+		list = listUpdated;
+	}
+</script>
+
+<List {list} on:delete={({ detail }) => deleteEntry(detail)} />
