a23452f0dc289a1e1b148dd0fd4a336ba8d71856
Belar <belar@belardesign.com>
2023-06-23 09:17:34 +0000

    feat: add focus fallback for an empty list

--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import Button from './Button.svelte';
+	import { tick } from 'svelte/internal';
 
 	const dispatch = createEventDispatcher();
 
@@ -9,15 +10,27 @@
 	};
 
 	export let list: Entry[] = [];
+	let emptyListRef: HTMLElement;
 
-	function dispatchDelete(entry: Entry) {
+	$: hasRecords = !!list.length;
+
+	async function dispatchDelete(entry: Entry) {
 		dispatch('delete', entry);
+
+		await tick();
+		if (!hasRecords) {
+			emptyListRef?.focus();
+		}
 	}
 </script>
 
-<ul>
-	{#each list as entry}
-		{@const { name } = entry}
-		<li>{name} <Button on:click={() => dispatchDelete(entry)}>Delete</Button></li>
-	{/each}
-</ul>
+{#if hasRecords}
+	<ul>
+		{#each list as entry}
+			{@const { name } = entry}
+			<li>{name} <Button on:click={() => dispatchDelete(entry)}>Delete</Button></li>
+		{/each}
+	</ul>
+{:else}
+	<p tabindex="-1" bind:this={emptyListRef}>List is empty</p>
+{/if}
